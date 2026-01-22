9c4e0e820d117f77e9002a8a6c048c7125db3867
GA34-hp <gillesalainwaffo@gmail.com>
2026-01-22 03:31:00 +0100

    railway configuration updates

--- server.ts
+++ server.ts
@@ -74,7 +74,8 @@ app.post('/api/emails', async (req: Request<unknown, ApiResponse, EmailData>, re
   }
 });
 
-const PORT = 3001;
+// const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}`);
 });
